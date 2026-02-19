3061a598cfa2a082d84149e3ee6e20c54eed9dd7
Aru <132051378+Aru-gxtx@users.noreply.github.com>
2026-02-19 20:43:54 +0800

    Update profiles.py

--- profiles.py
+++ profiles.py
@@ -1,3 +1,16 @@
+"""
+Write a program that grabs the full HTML from the page at the URL http://olympus.realpython.org/profiles.
+
+Using Beautiful Soup, print out a list of all the links on the page by looking for HTML tags with the name a and retrieving the value taken on by the href attribute of each tag.
+
+The final output should look like this:
+
+http://olympus.realpython.org/profiles/aphrodite
+http://olympus.realpython.org/profiles/poseidon
+http://olympus.realpython.org/profiles/dionysus
+Make sure that you only have one slash (/) between the base URL and the relative URL.
+"""
+
 from urllib.request import urlopen
 
 url = "http://olympus.realpython.org/profiles"
